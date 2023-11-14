87ff7a891f638437a12ca3ee8230c5d4a16a2ba9
ahmedalaeldin <alaa.mido515@yahoo.com>
2023-11-14 10:51:36 -0800

    newproject

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
+
+#endif
