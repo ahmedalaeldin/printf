7677b06062782c555b9805a3ca5243f2ddd9b606
ahmedalaeldin <alaa.mido515@yahoo.com>
2023-11-14 11:44:00 -0800

    newproject

--- main.h
+++ main.h
@@ -1,12 +1,6 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef _PRINTF_H
+#define _PRINTF_H
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _strlen(char *str);
-int _strlenc(const char *str);
-int print_37(void);
+#include <stdarg.h>
+#include <stdio.h>
 
-#endif
