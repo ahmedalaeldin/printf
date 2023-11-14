1163baf37bf92651ad97e29ad64edee6d29dbff0
ahmedalaeldin <alaa.mido515@yahoo.com>
2023-11-14 14:27:36 -0800

    newproject

--- _put.c
+++ _put.c
@@ -25,14 +25,14 @@ int _puts(char *str)
 int _putchar(int c)
 {
 	static int i;
-	static char buf[OUTPUT_BUF_SIZE]
+	static char buf[OUTPUT_BUF_SIZE];
 
 	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
 	{
-	 	write(l, buf, i);
+		write(1, buf, i);
 		i = 0;
 	}
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
-	return (l);
+	return (1);
 }
